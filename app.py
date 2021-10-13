094e076a5372269eed3817584709b31cb5c18a78
fabianojdp <71109443+fabianojdp@users.noreply.github.com>
2021-10-13 03:02:33 -0300

    Correção para visualizar os dados Correção do app.py e alteração do index.html

--- app.py
+++ app.py
@@ -4,23 +4,23 @@ import sqlite3
 from flask_sqlalchemy import SQLAlchemy
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_file = "sqLite:///{}".format(os.path.join(project_dir, "database.db"))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "database.db"))
 
 app = Flask('__name__')
 app.config['SECRET_KEY'] = 'Senha*659'
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 db = SQLAlchemy(app)
 
-''' class controle_ferias(db.Model):
+class Controle_ferias(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    inclusao = db.Column(db.Datetime, default=datetime.datetime.utcnow)
-    empresaid = db.Column(db.Integer(3), nulltable=False)
-    pessoa = db.Column(db.String(60), nulltable=False)
-    data_inicio_ferias = db.Column(db.Date, nulltable=False)
-    data_fim_ferias = db.Column(db.Date, nulltable=False)
-    valor_ferias = db.Column(db.Decimal(6,2), nulltable=False) '''
+    inclusao = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    empresaid = db.Column(db.Integer, nullable=False)
+    pessoa = db.Column(db.String(60), nullable=False)
+    data_inicio_ferias = db.Column(db.Date, nullable=False)
+    data_fim_ferias = db.Column(db.Date, nullable=False)
+    valor_ferias = db.Column(db.Float, nullable=False)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    cf = Controle_ferias.query.all()
+    return render_template('index.html', cf=cf)
--- init.db.py
+++ init.db.py
@@ -3,8 +3,14 @@ from sqlite3.dbapi2 import connect
 
 connection = sqlite3.connect('database.db')
 
-with open ('schame.sql') as f:
-    connection.executescript(f.read())
+''' with open ('schema.sql') as f:
+    connection.executescript(f.read()) '''
+
+cur = connection.cursor()
+
+cur.execute("INSERT INTO controle_ferias(inclusao, empresaid, pessoa, data_inicio_ferias, data_fim_ferias, valor_ferias) VALUES (?, ?)",
+            ('13/10/2021', '01', 'SÉRGIO CARLOS', '25/10/2021', '24/11/2021', '1670.50')
+            )
 
 cur = connection.cursor()
 
