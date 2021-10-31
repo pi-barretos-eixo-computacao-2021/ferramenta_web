267f4693509e83efb519be567324b9af0860671b
fabianojdp <71109443+fabianojdp@users.noreply.github.com>
2021-10-31 02:01:06 -0300

    Correções na View, novo.html, editar.html

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import sqlite3
 import locale
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.exceptions import abort
+from datetime import date
+# from datetime import date, datetime
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "database.db"))
@@ -21,8 +23,13 @@ class Controle_ferias(db.Model):
     inclusao = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     empresaid = db.Column(db.Integer, nullable=False)
     pessoa = db.Column(db.String(60), nullable=False)
-    data_inicio_ferias = db.Column(db.Date, nullable=False)
-    data_fim_ferias = db.Column(db.Date, nullable=False)
+    data_inicio_ferias = db.Column(db.Date)
+    data_fim_ferias = db.Column(db.Date)
+    # data_inicio_ferias = db.Column(strftime("%m/%d/%Y"))
+    # data_fim_ferias = db.Column(strftime("%m/%d/%Y"))
+
+    # data_inicio_ferias = db.DateTime(db.DateTime)
+    # data_fim_ferias = db.DateTime(db.DateTime)
     valor_ferias = db.Column(db.Float, nullable=False)
 
 @app.route('/')
@@ -43,12 +50,13 @@ def cf2(cf2_id):
 
 @app.route('/novo', methods=('GET', 'POST'))
 def novo():
-    if request.method == 'POST':        
-        empresaid = request.form['empresaid']
-        pessoa = request.form['pessoa']
-        data_inicio_ferias = request.form['data_inicio_ferias']
-        data_fim_ferias = request.form['pesdata_fim_feriassoa']                
-        valor_ferias = request.form['valor_ferias']        
+    if request.method == 'POST':
+        empresaid = request.form.get('empresaid')
+        pessoa = request.form.get('pessoa')
+        #data_inicio_ferias=request.form.get('data_inicio_ferias')
+        data_inicio_ferias=request.form.get('db.DateTime')
+        data_fim_ferias=request.form.get('db.DateTime')
+        valor_ferias=request.form.get('valor_ferias').replace(',', '.')
 
         if not pessoa:
             flash('Campo obrigatório')
@@ -67,17 +75,19 @@ def editar(cf2_id):
     if request.method == 'POST':
         empresaid = request.form['empresaid']
         pessoa = request.form['pessoa']
+        # data_inicio_ferias = request.form['data_inicio_ferias']
         data_inicio_ferias = request.form['data_inicio_ferias']
         data_fim_ferias = request.form['data_fim_ferias']                
-        valor_ferias = request.form['valor_ferias']
+        valor_ferias = request.form['valor_ferias'].replace(',', '.')
 
         if not pessoa:
             flash('Campo obrigatório')
         else:
             cf2.empresaid = empresaid
             cf2.pessoa = pessoa
-            cf2.data_inicio_ferias = data_inicio_ferias            
-            cf2.pesdata_fim_ferias = data_fim_ferias
+            # cf2.data_inicio_ferias = data_inicio_ferias
+            data_inicio_ferias=request.form.get('db.DateTime')
+            data_fim_ferias=request.form.get('db.DateTime')
             cf2.valor_ferias = valor_ferias
             db.session.commit()
             return redirect(url_for('index'))
